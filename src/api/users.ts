60fbb8cbc91cb36b47c8f35775efbd2cc3618edf
akshelstad <akshelstad@gmail.com>
2025-09-28 19:02:38 -0600

    updated users handler

--- src/api/users.ts
+++ src/api/users.ts
@@ -2,21 +2,30 @@ import { Request, Response } from "express";
 
 import { respondWithJSON } from "./json.js";
 import { createUser } from "../lib/db/queries/users.js";
-import { NewUser } from "../lib/db/schema.js";
+import { NewUser, User } from "../lib/db/schema.js";
 import { BadRequestError, InternalServerError } from "./errors.js";
+import { hashPassword } from "src/lib/auth/auth.js";
+
+type userSafe = Omit<User, "hashedPassword">;
 
 export async function handlerAddUser(req: Request, res: Response) {
   type parameters = {
     email: string;
     password: string;
   };
+
   const params: parameters = req.body;
 
   if (!params.email || !params.password) {
     throw new BadRequestError("missing required fields for user");
   }
 
-  const user: NewUser = await createUser({ email: params.email });
+  const hashedPassword = await hashPassword(params.password);
+
+  const user: userSafe = await createUser({
+    email: params.email,
+    hashedPassword,
+  });
 
   if (!user) {
     throw new InternalServerError("unable to create user");
